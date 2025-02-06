fc0d6b7d499f26aa97f2f175df0a70c5ed7aeef1
Rintarooo <310rnomeado@gmail.com>
2025-02-06 16:24:51 +0900

    add outputs tf

--- infra/terraform/envs/dev/lambda/outputs.tf
+++ infra/terraform/envs/dev/lambda/outputs.tf
@@ -1,24 +1,24 @@
 output "ecr_repository_name" {
   description = "Name of the ECR repository"
-  value       = aws_ecr_repository.lambda_repository.name
+  value       = module.lambda.ecr_repository_name
 }
 
 output "lambda_function_name" {
   description = "Name of the Lambda function"
-  value       = aws_lambda_function.function.function_name
+  value       = module.lambda.lambda_function_name
 }
 
 output "lambda_function_arn" {
   description = "ARN of the Lambda function"
-  value       = aws_lambda_function.function.arn
+  value       = module.lambda.lambda_function_arn
 }
 
 output "lambda_function_image_uri" {
   description = "Image URI of the Lambda function"
-  value       = aws_lambda_function.function.image_uri
+  value       = module.lambda.lambda_function_image_uri
 }
 
 output "lambda_function_role_arn" {
   description = "ARN of the Lambda function's IAM role"
-  value       = aws_iam_role.lambda_role.arn
+  value       = module.lambda.lambda_function_role_arn
 }
