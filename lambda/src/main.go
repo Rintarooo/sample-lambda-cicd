ff82ee7336515b879777e1050aaeed960e1a42e4
Rintarooo <310rnomeado@gmail.com>
2025-02-07 18:01:10 +0900

    add test (#20)
    * add test
    
    * handle error

--- lambda/src/main.go
+++ lambda/src/main.go
@@ -2,23 +2,54 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"errors"
+	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Event struct {
+type Request struct {
 	Name string `json:"name"`
+}
+
+func (r *Request) Validate() error {
+	if r.Name == "" {
+		return errors.New("error: invalid request. name is required")
+	}
+	return nil
 }
 
 type Response struct {
 	Message string `json:"message"`
 }
 
-func handler(ctx context.Context, event Event) (Response, error) {
-	return Response{
-		Message: fmt.Sprintf("Hello %s from Lambda!", event.Name),
+func newLambdaResponse(body string, statusCode int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: statusCode,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: body,
 	}, nil
+}
+
+func handler(ctx context.Context, req Request) (events.APIGatewayProxyResponse, error) {
+	if err := req.Validate(); err != nil {
+		return newLambdaResponse(err.Error(), http.StatusBadRequest)
+	}
+
+	response := Response{
+		Message: "Hello " + req.Name + " from Lambda!",
+	}
+
+	body, err := json.Marshal(response)
+	if err != nil {
+		return newLambdaResponse(err.Error(), http.StatusInternalServerError)
+	}
+
+	return newLambdaResponse(string(body), http.StatusOK)
 }
 
 func main() {
